40c8c417b14e3a7f183fdbf5cb8d9aa504077dc8
Demonept <66496493+Demonept@users.noreply.github.com>
2022-12-21 10:40:40 +0300

    Контроллер работает через интерфейс КарСервис

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.Service.CarService;
 import web.Service.CarServiceImpl;
 import web.model.Car;
 
@@ -15,7 +16,7 @@ import java.util.List;
 @Controller
 public class CarsController {
 
-    private CarServiceImpl carService;
+    private CarService carService;
 
     @Autowired
     public CarsController(CarServiceImpl carService){
